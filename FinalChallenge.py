17827f204cabce931b08adb3cfe3c8f42788ddd7
carlosezmz <39721561+carlosezmz@users.noreply.github.com>
2020-05-05 14:09:50 -0400

    suing dataframe

--- FinalChallenge.py
+++ FinalChallenge.py
@@ -4,7 +4,7 @@
 
 # main
 from pyspark import SparkContext
-# from pyspark.sql.session import SparkSession
+from pyspark.sql.session import SparkSession
 
 
 
@@ -92,6 +92,9 @@ def check_house_number(number):
                 except ValueError:
                     number = number
                     
+    elif type(number) == int:
+        number = number
+                    
     if type(number) == list:
         number = number[0]
                     
@@ -100,7 +103,7 @@ def check_house_number(number):
 def check_county(county):
     
     boro_dict = {
-        'ny':1, 'ne': 1, 'ma':1, 'mn':1, 'mh':1,
+        'ny':1, 'ne': 1, 'ma':1, 'mn':1,
         'bx':2, 'br':2,
         'k': 3, 'ki':3, 'bk':3,
         'q': 4, 'qn':4, 'qu':4, 
@@ -130,92 +133,100 @@ def extract_cols(partId, records):
     
     if partId==0:
         next(records)
-    
-    from datetime import datetime
     import csv
-    
     reader = csv.reader(records)
     for row in reader:
         
         if len(row) == 43:
-            
-            try:
         
-                county = row[21]
-                number = str(row[23])
-                year = datetime.strptime(row[4], '%m/%d/%Y').year
-                st_name = row[24].upper()
-                st_intc = row[25].upper()
+            county = row[21]
+            number = str(row[23])
+            year = int(row[4][-4:])
+            st_name = row[24].upper()
+            st_intc = row[25].upper()
         
             # chechk for county
-                if county:
-                    county = county.lower()
-                    county = check_county(county)
+            if county:
+                county = county.lower()
+                county = check_county(county)
         
 #                 # check for house number
-#                 number = check_house_number(number)
+                number = check_house_number(number)
         
-                    # check for street name
-                    st_name = check_name(st_name)
+                # check for street name
+                st_name = check_name(st_name)
         
-                    # check intersection name
-                    st_intc = check_int_street(st_intc)
+                # check intersection name
+                st_intc = check_int_street(st_intc)
     
         
-                    if year in [2015, 2016, 2017, 2018, 2019]:
+                if year in [2015, 2016, 2017, 2018, 2019]:
             
-#                     if type(number) != str:
+                    if type(number) != str:
                 
                         if county:
             
-                            yield (county, (year, st_name, number, st_intc))
+                            yield (county, st_name, year, number)
+
                 
-            except ValueError:
-                continue
-                
-                
-def extract_segment(partId, records):
-    
-    if partId==0:
-        next(records)
-    import csv
-    reader = csv.reader(records)
-    
-    for row in reader:
+def get_phyID(partID, records):
+
+    for row in records:
         
-        phy_id = int(row[0])
-        boro = int(row[13])
-        st_name = row[28]
+        year = row['year']
+        st_numb = row['st_numb']
+        phy_id = row['phy_id']
+        l_low = check_house_number(row['l_low'])
+        l_hig = check_house_number(row['l_hig'])
         
-        
-        if boro and row[1]:
-        
-            yield (boro, (st_name, phy_id, row[1], row[3], row[4], row[5]))
+        if (type(st_numb) == type(l_low)) & (type(st_numb) == type(l_hig)):
             
+            if (st_numb >= l_low) & (st_numb <= l_hig):
+                
+                yield (phy_id, 1)
+    
+    
 def run_spark(sc, fie_dir, center_dir):
     
     
     parking_violations = sc.textFile(fie_dir)\
-               .mapPartitionsWithIndex(extract_cols)
+                           .mapPartitionsWithIndex(extract_cols)
     
-    center_line = sc.textFile(center_dir)\
-                .mapPartitionsWithIndex(extract_segment)
+    parking_violations = spark.createDataFrame(parking_violations, schema=['boro', 'st_name', 'year', 'st_numb'])
     
-    parking_violations = parking_violations.join(center_line).values()\
-                                    .filter(lambda x: x[0][1] == x[1][0])\
-                                    .map(lambda x: (x[0][0], x[0][1], x[0][2], x[1][1], x[1][2], x[1][3], x[1][4], x[1][5]))\
-                                    .filter(lambda x: (x[2] >= x[4] and x[2] <= x[5]) or (x[2] >= x[6] and x[2] <= x[7]))\
-                                    .map(lambda x: (x[3], 1))\
-                                    .reduceByKey(lambda x,y: x+y)\
-                                    .sortByKey()
+    
+    parking_violations = parking_violations.join(center_line, 
+                        [parking_violations.boro == center_line.boro, violations.st_name == center_line.st_name], 
+                        'inner')
+    
+    parking_violations = parking_violations.select('year', 'st_numb', 'phy_id', 'l_low', 'l_hig')
+    
+    parking_violations = parking_violations.rdd.mapPartitionsWithIndex(get_phyID)\
+                                               .reduceByKey(lambda x,y: x+y)\
+                                               .sortByKey()
+
     
     return parking_violations
             
             
             
-def main(sc):
+def main(sc, spark):
     
     center_dir = 'hdfs:///data/share/bdm/nyc_cscl.csv'
+    
+    center_line = spark.read.load(center_dir, format='csv', header=True, inferSchema=True)
+    
+    center_line = center_line.select(
+    center_line['PHYSICALID'].cast('int').alias('phy_id'),
+    center_line['L_LOW_HN'].alias('l_low'),
+    center_line['L_HIGH_HN'].alias('l_hig'),
+    center_line['BOROCODE'].cast('int').alias('boro'),
+    center_line['FULL_STREE'].alias('st_name'))
+    
+    center_line = center_line.filter((center_line['l_low'].isNotNull())\
+                                     & (center_line['l_hig'].isNotNull())\
+                                     & (center_line['boro'].isNotNull())\
+                                     & (center_line['st_name'].isNotNull()))
     
     fie2015_dir = 'hdfs:///data/share/bdm/nyc_parking_violation/2015.csv'
     fie2016_dir = 'hdfs:///data/share/bdm/nyc_parking_violation/2016.csv'
@@ -234,7 +245,8 @@ def main(sc):
     parking_violations = parking_violations.join(parking_violations_2018)
     parking_violations = parking_violations.join(parking_violations_2019)
     
-    parking_violations = parking_violations.mapValues(lambda x: (x[0], x[1], x[2], x[3], x[4], (x[4]-x[0])/4))
+    parking_violations = parking_violations.mapValues(lambda x: (x[0], x[1], x[2], x[3], x[4], 
+                                                                 ((x[4]-x[3]) + (x[3]-x[2]) + (x[2]-x[1]) + (x[1]-x[0]))/4))
     
     parking_violations.saveAsTextFile('hdfs:////user/ctavare003/parkingViolation_count')
 
@@ -242,6 +254,7 @@ def main(sc):
 if __name__ == '__main__':
     
     sc = SparkContext()
+    spark = SparkSession(sc)
     
-    main(sc)
+    main(sc, spark)
         
