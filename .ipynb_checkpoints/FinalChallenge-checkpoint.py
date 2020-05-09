ce93b613bb091ff40f9f17894351bc6418e95dc2
carlosezmz <39721561+carlosezmz@users.noreply.github.com>
2020-05-09 01:21:31 -0400

    ss

--- .ipynb_checkpoints/FinalChallenge-checkpoint.py
+++ .ipynb_checkpoints/FinalChallenge-checkpoint.py
@@ -369,7 +369,7 @@ if __name__ == '__main__':
     center_dir = 'hdfs:///data/share/bdm/nyc_cscl.csv'
     
     center_line = sc.textFile(center_dir)\
-                .mapPartitionsWithIndex(extract_bounds)
+                .mapPartitionsWithIndex(extract_bounds).collect()
     
 #     center_line = spark.read.load(center_dir, format='csv', header=True, inferSchema=True)
     
@@ -394,7 +394,7 @@ if __name__ == '__main__':
 #     files_list = [fie2015_dir, fie2016_dir, fie2017_dir, fie2018_dir, fie2019_dir]
     
     parking_violations = sc.textFile(fie2015_dir)\
-                .mapPartitionsWithIndex(extract_cols)
+                .mapPartitionsWithIndex(extract_cols).collect()
     
     parking_violations = run_spark(parking_violations, center_line)
 #     parking_violations_2016 = run_spark(sc, fie2016_dir)
@@ -412,5 +412,5 @@ if __name__ == '__main__':
     
     
     
-    parking_violations.saveAsTextFile('/user/ctavare003/Violation_count')
+    parking_violations.saveAsTextFile('/user/ctavare003/Violations')
 
