f8d799900cccc305ca0c9040b662a9ef7911b83a
carlosezmz <39721561+carlosezmz@users.noreply.github.com>
2020-05-11 16:14:56 -0400

    gg

--- FinalChallenge.py
+++ FinalChallenge.py
@@ -418,11 +418,11 @@ if __name__ == '__main__':
             
             rdd = sc.textFile(file)\
                     .mapPartitionsWithIndex(extract_cols)\
-                    .filter(lambda x: x[1][3] == year)
+                    .filter(lambda x: x[1][3] == year).cache()
             
-            rdd = rdd.join(bounds)\
-                     .values()\
-                     .mapPartitionsWithIndex(get_id).cache()
+#             rdd = rdd.join(bounds)\
+#                      .values()\
+#                      .mapPartitionsWithIndex(get_id).cache()
             
             parking_violations = rdd
             
@@ -430,18 +430,21 @@ if __name__ == '__main__':
             
             rdd = sc.textFile(file)\
                     .mapPartitionsWithIndex(extract_cols)\
-                    .filter(lambda x: x[1][3] == year)
+                    .filter(lambda x: x[1][3] == year).cache()
             
-            rdd = rdd.join(bounds)\
-                     .values()\
-                     .mapPartitionsWithIndex(get_id).cache()
+#             rdd = rdd.join(bounds)\
+#                      .values()\
+#                      .mapPartitionsWithIndex(get_id).cache()
             
             parking_violations = parking_violations.union(rdd).cache()
             
     
 #     parking_violations = parking_violations.distinct().cache()
     
-    parking_violations = parking_violations.reduceByKey(lambda x,y: (x[0]+y[0], x[1]+y[1], x[2]+y[2], x[3]+y[3], x[4]+y[4]))\
+    parking_violations = parking_violations.join(bounds)\
+                                           .values()\
+                                           .mapPartitionsWithIndex(get_id)\
+                                           .reduceByKey(lambda x,y: (x[0]+y[0], x[1]+y[1], x[2]+y[2], x[3]+y[3], x[4]+y[4]))\
                                            .mapValues(lambda x: (x[0],x[1],x[2],x[3],x[4],((x[4]-x[3])+(x[3]-x[2])+(x[2]-x[1])+(x[1]-x[0]))/4))\
                                            .sortByKey().cache()
     
