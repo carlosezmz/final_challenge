c2360df5bbaf1132ae3fd664908423cf8ad0140b
carlosezmz <39721561+carlosezmz@users.noreply.github.com>
2020-05-11 12:39:40 -0400

    cache2

--- FinalChallenge.py
+++ FinalChallenge.py
@@ -411,7 +411,7 @@ if __name__ == '__main__':
                                            .mapPartitionsWithIndex(get_id)\
                                            .reduceByKey(lambda x,y: (x[0]+y[0], x[1]+y[1], x[2]+y[2], x[3]+y[3], x[4]+y[4]))\
                                            .mapValues(lambda x: (x[0],x[1],x[2],x[3],x[4],((x[4]-x[3])+(x[3]-x[2])+(x[2]-x[1])+(x[1]-x[0]))/4))\
-                                           .sortByKey()
+                                           .sortByKey().cache()
     
 #     parking_violations = parking_violations.reduceByKey(lambda x,y: (x[0]+y[0], x[1]+y[1], x[2]+y[2], x[3]+y[3], x[4]+y[4]))\
 #                                            .mapValues(lambda x: (x[0], x[1], x[2], x[3], x[4], 
