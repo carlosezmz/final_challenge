d6be22c13d321dde39a4a7a0ec6cf6fdee3cae89
carlosezmz <39721561+carlosezmz@users.noreply.github.com>
2020-05-16 12:21:07 -0400

    ss

--- FinalChallenge.py
+++ FinalChallenge.py
@@ -468,6 +468,7 @@ if __name__ == '__main__':
     parking_violations = sc.textFile(fie2015_dir)\
                            .mapPartitionsWithIndex(extract_cols)\
                            .join(bounds).values().filter(lambda x: (x[0][0] >= x[1][1]) & (x[0][0] <= x[1][2]))\
+                           .sortByKey()\
                            .mapPartitionsWithIndex(reduce_csv)\
                            .saveAsTextFile('parkingCount')
 
